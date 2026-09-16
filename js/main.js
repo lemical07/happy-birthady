6bf977724428901ca805c78bda9c83cdb90aa225
lemical07 <icaleladio@gmail.com>
2026-09-16 17:21:49 -0600

    fix: corregir tiempos de transición y cambiar botón comenzar por imagen

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
 const intro = document.querySelector('.home__intro');
 const surprise = document.querySelector('.home__surprise');
 const reveal = document.querySelector('.home__reveal');
-
 const box = document.querySelector('.home__box');
-
 const celebration = document.querySelector('.home__celebration');
+
 const celebrationVideoContainer = document.querySelector('.home__celebration-video');
 const celebrationVideo = document.querySelector('.home__celebration-media');
 
@@ -42,8 +41,5 @@ celebrationVideo.addEventListener('ended', () => {
     setTimeout(() => {
         celebration.style.display = 'none';
         reveal.style.display = 'flex';
-        requestAnimationFrame(() => {
-            reveal.classList.add('is-visible');
-        });
-    }, 1500);
+    }, 300);
 });
